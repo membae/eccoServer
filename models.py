262c431302778d22fa4827dbd36a2b5c13a274d6
membae <membaphilip1@gmail.com>
2026-02-01 09:33:18 +0300

    balance model

--- models.py
+++ models.py
@@ -12,7 +12,18 @@ class User(db.Model,SerializerMixin):
     id=db.Column(db.Integer, primary_key=True)
     full_name=db.Column(db.String, nullable=False)
     email=db.Column(db.String, unique=True, nullable=False)
-    phone_number=db.Column(db.Integer, nullable=False)
-    password=db.Column(db.Integer, nullable=False)
+    phone_number=db.Column(db.String, nullable=False)
+    password=db.Column(db.String, nullable=False)
     country=db.Column(db.String, nullable=False)
     role=db.Column(db.String, default="user", nullable=False)
+    
+    balance = db.relationship('Balance', back_populates='user', uselist=False)
+    
+class Balance(db.Model,SerializerMixin):
+    __tablename__="balances"
+    
+    id=db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Float, nullable=False, default=0.0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True, nullable=False)
+    
+    user = db.relationship('User', back_populates='balance')
